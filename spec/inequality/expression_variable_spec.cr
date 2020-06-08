17d76d4017aaa4c5971f26d0e069da56d2a9e3fa
Joel <joel@neutrinographics.com>
2020-06-08 20:39:30 +0700

    added expression variable inequality test

---
+++ spec/inequality/expression_variable_spec.cr
@@ -0,0 +1,50 @@
+require "../spec_helper"
+
+describe Kiwi do
+  it "checks that a variable is less than or equal to a value" do
+    solver = Kiwi::Solver.new
+    x = Kiwi::Variable.new("x")
+    solver.add_constraint(Kiwi::Symbolics.less_than_or_equal_to(Kiwi::Expression.new(100), x))
+    solver.update_variables
+    (100 <= x.value).should be_truthy
+    solver.add_constraint(Kiwi::Symbolics.equals(x, 110))
+    solver.update_variables
+    x.value.should be_close(110.0, EPSILON)
+  end
+
+  it "is unable to satisfy a less than or equal to constraint" do
+    solver = Kiwi::Solver.new
+    x = Kiwi::Variable.new("x")
+    solver.add_constraint(Kiwi::Symbolics.less_than_or_equal_to(Kiwi::Expression.new(100), x))
+    solver.update_variables
+    (x.value <= 100).should be_truthy
+
+    expect_raises(Kiwi::UnsatisfiableConstraintException) do
+      solver.add_constraint(Kiwi::Symbolics.equals(x, 10))
+      solver.update_variables
+    end
+  end
+
+  it "check that a variable is greater than or equal to a value" do
+    solver = Kiwi::Solver.new
+    x = Kiwi::Variable.new("x")
+    solver.add_constraint(Kiwi::Symbolics.greater_than_or_equal_to(Kiwi::Expression.new(100), x))
+    solver.update_variables
+    (100 >= x.value).should be_truthy
+    solver.add_constraint(Kiwi::Symbolics.equals(x, 90))
+    solver.update_variables
+    x.value.should be_close(90, EPSILON)
+  end
+
+  it "is unable to satisfy a greater than or equal to constraint" do
+    solver = Kiwi::Solver.new
+    x = Kiwi::Variable.new("x")
+    solver.add_constraint(Kiwi::Symbolics.greater_than_or_equal_to(Kiwi::Expression.new(100), x))
+    solver.update_variables
+    (100 >= x.value).should be_truthy
+    expect_raises(Kiwi::UnsatisfiableConstraintException) do
+      solver.add_constraint(Kiwi::Symbolics.equals(x, 110))
+      solver.update_variables
+    end
+  end
+end
--- spec/inequality/variable_variable_spec.cr
+++ spec/inequality/variable_variable_spec.cr
@@ -3,13 +3,15 @@ require "../spec_helper"
 describe Kiwi do
   it "checks that a variable is less than or equal to a value" do
     x = Kiwi::Variable.new("x")
+    y = Kiwi::Variable.new("y")
     solver = Kiwi::Solver.new
-    solver.add_constraint(Kiwi::Symbolics.less_than_or_equal_to(100, x))
+    solver.add_constraint(Kiwi::Symbolics.equals(y, 100))
+    solver.add_constraint(Kiwi::Symbolics.less_than_or_equal_to(x, y))
     solver.update_variables
     (100 <= x.value).should be_truthy
-    solver.add_constraint(Kiwi::Symbolics.equals(x, 110))
+    solver.add_constraint(Kiwi::Symbolics.equals(x, 90))
     solver.update_variables
-    x.value.should be_close(110, EPSILON)
+    x.value.should be_close(90, EPSILON)
   end
 
   it "is unable to satisfy a less than or equal to constraint" do
