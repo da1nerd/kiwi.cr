dce0c59c6c5f01ae7f77cb40cf47e8c7bd351216
Joel <joel@neutrinographics.com>
2021-01-06 23:25:16 +0700

    added test for edit variables

---
+++ spec/edit_variables_spec.cr
@@ -0,0 +1,21 @@
+require "./spec_helper"
+
+describe Kiwi do
+  it "suggests a new variable value" do
+    solver = Kiwi::Solver.new
+    x = Kiwi::Variable.new("x")
+    y = Kiwi::Variable.new("y")
+    solver.add_constraint(x == y * 2)
+    solver.add_edit_variable(x, Kiwi::Strength::STRONG)
+
+    solver.suggest_value(x, 4)
+    solver.update_variables
+    x.value.should eq(4)
+    y.value.should eq(2)
+
+    solver.suggest_value(x, 8)
+    solver.update_variables
+    x.value.should eq(8)
+    y.value.should eq(4)
+  end
+end
